0ac4a170e2f7a756573e139b4bf8ddf91dd08e65
saraponton <sara.ponton@hotmail.com>
2018-09-19 14:17:40 +0200

    change 3

--- hmw2.py
+++ hmw2.py
@@ -1,12 +1,10 @@
 import math
 
-
-prompt = "Please enter the first number "
-num1 = input(prompt)
-prompt2 = "Please enter the second number "
-num2 = input(prompt2)
+num1 = input("Please enter the first number ")
 num1 = float(num1)
+num2 = input("Please enter the second number ")
 num2 = float(num2)
+
 
 num3 = int(math.ceil(num1/num2))
 print("The result of the division is: " + str(num3))
