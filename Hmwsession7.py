a3a0efcaaff7e0e2b628dc884579054aa1bc1ecd
saraponton <42964901+saraponton@users.noreply.github.com>
2018-11-05 17:49:21 +0100

    Update Hmwsession7.py

--- Hmwsession7.py
+++ Hmwsession7.py
@@ -1,4 +1,6 @@
-#Write a program that prints the longest substring of s in which the letters occur in alphabetical order. For example, if s = 'azcbobobegghakl', then your program should print:Longest substring in alphabetical order is: begghIn the case of ties, print the first substring. For example, if s = 'abcbcd', then your program should print: Longest substring in alphabetical order is: abc
+#Write a program that prints the longest substring of s in which the letters occur in alphabetical order. 
+#For example, if s = 'azcbobobegghakl', then your program should print:Longest substring in alphabetical order is: begghIn the case of ties, print the first substring. 
+#For example, if s = 'abcbcd', then your program should print: Longest substring in alphabetical order is: abc
 
 string = input("Hi, please enter a string of characters ")
 solution = ""
