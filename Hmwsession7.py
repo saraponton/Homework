7eb869e402f02449a3f860a72eef2ec25154b8fb
saraponton <42964901+saraponton@users.noreply.github.com>
2018-11-05 17:50:09 +0100

    Update Hmwsession7.py

--- Hmwsession7.py
+++ Hmwsession7.py
@@ -2,10 +2,11 @@
 #For example, if s = 'azcbobobegghakl', then your program should print:Longest substring in alphabetical order is: begghIn the case of ties, print the first substring. 
 #For example, if s = 'abcbcd', then your program should print: Longest substring in alphabetical order is: abc
 
-string = input("Hi, please enter a string of characters ")
 solution = ""
 string1 = ""
 alphabet = "a"
+
+string = input("Hi, please enter a string of characters ")
 
 for c in string:
     if c>= alphabet:
